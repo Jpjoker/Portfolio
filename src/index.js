ad351875898af587cc7a639bd8e50d0b9170160f
jurgen <133093619+Jpjoker@users.noreply.github.com>
2024-03-17 21:31:27 +0100

    text-animation werkt ongeveer 90%

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 'use strict';
 
-alert('Dit is een alert!');
-console.log('Welkom bij Webpack!');
+
+/*animation */ 
+const phrases = [
+  "Full Stack Developer",
+  "Ethical Hacker",
+  "Cybersecurity",
+  "Computer Science",
+  "Network Engineer"
+];
+
+const textAnimationSpan = document.querySelector('.text-animation span');
+let phraseIndex = 0;
+
+const cyclePhrases = () => {
+  textAnimationSpan.textContent = phrases[phraseIndex++ % phrases.length];
+  setTimeout(cyclePhrases, 5000); // Verander de tekst elke 4 seconden
+};
+
+cyclePhrases(); // Start de cyclus
 
 
-
-
-
-
-module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      // ... andere regels voor andere bestandstypen
-    ]
-  }
   
