a8597553e1ece4431083ce6cb541839246e8bd5f
jurgen <133093619+Jpjoker@users.noreply.github.com>
2024-03-17 22:13:25 +0100

    about us page done

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-'use strict';
+// 'use strict';
 
 
-/*animation */ 
-const phrases = [
-  "Full Stack Developer",
-  "Ethical Hacker",
-  "Cybersecurity",
-  "Computer Science",
-  "Network Engineer"
-];
+// /*animation for the home */ 
+// const phrases = [
+//   "Full Stack Developer",
+//   "Ethical Hacker",
+//   "Cybersecurity",
+//   "Computer Science",
+//   "Network Engineer"
+// ];
 
-const textAnimationSpan = document.querySelector('.text-animation span');
-let phraseIndex = 0;
+// const textAnimationSpan = document.querySelector('.text-animation span');
+// let phraseIndex = 0;
 
-const cyclePhrases = () => {
-  textAnimationSpan.textContent = phrases[phraseIndex++ % phrases.length];
-  setTimeout(cyclePhrases, 5000); // Verander de tekst elke 4 seconden
-};
+// const cyclePhrases = () => {
+//   textAnimationSpan.textContent = phrases[phraseIndex++ % phrases.length];
+//   setTimeout(cyclePhrases, 3000); // Verander de tekst elke 4 seconden
+// };
 
-cyclePhrases(); // Start de cyclus
+// cyclePhrases(); // Start de cyclus
 
 
   
