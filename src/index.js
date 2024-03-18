043d1adc7a5e902a633635c9579e39ad475bff42
jurgen <133093619+Jpjoker@users.noreply.github.com>
2024-03-18 13:32:02 +0100

    menu gemaakt in JS check

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 'use strict';
 
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('#navbar');
 
-// /*animation for the home */ 
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height){
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector(`header nav a[href*=${id}]`).classList.add('active');
+            });
+        }
+    }); // Sluitende haakjes toegevoegd hier
+} // Sluitende accolade toegevoegd hier
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+// /*animation for the home en about */ 
 // const phrases = [
 //   "Full Stack Developer",
 //   "Ethical Hacker",
